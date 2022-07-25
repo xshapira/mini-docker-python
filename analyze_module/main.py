3a7124541702f33ea8ccecc7c271930411830bfe
Max Shapira <maxshapira9@gmail.com>
2022-07-25 21:02:53 +0300

    initial commit

---
+++ analyze_module/main.py
@@ -0,0 +1,9 @@
+import logging
+import messageBroker
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    logger.info("Analyze module is listening...")
+    # TODO
---
+++ password_module/main.py
@@ -0,0 +1,10 @@
+import logging
+
+import messageBroker
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    logger.info("Password module is listening...")
+    # TODO
