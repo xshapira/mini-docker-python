78a329dcfdf4ef6f112528ad8652a8e5a663ab9c
Max Shapira <maxshapira9@gmail.com>
2022-07-29 19:48:10 +0300

    find the number of files from each type and sort by size

--- analyze_module/main.py
+++ analyze_module/main.py
@@ -1,7 +1,9 @@
 import glob
 import logging
 import os
-from os.path import dirname, join
+import pathlib
+from collections import Counter
+from os.path import join
 
 # import messageBroker
 
@@ -11,3 +13,32 @@ from os.path import dirname, join
 # if __name__ == "__main__":
 #     logger.info("Analyze module is listening...")
 #     # TODO
+
+
+dir_path = join(pathlib.Path(), "theHarvester")
+data_path = glob.glob(f"{dir_path}/**/*", recursive=True)
+files = [i for i in data_path if os.path.isfile(i)]
+
+final_files = {}
+counts = Counter()
+
+for file in files:
+    file_type = os.path.splitext(file)[1]
+
+    if "." in file_type:
+        counts[file_type] += 1
+
+for file_type, count in counts.items():
+    final_files.update({file_type: count})
+
+# Sort the top 10 files by size
+files_ascending = sorted(files, key=lambda x: os.stat(x).st_size, reverse=True)[:10]
+
+for path_of_file in files_ascending:
+    size_of_file = os.stat(path_of_file).st_size
+    final_files.update({path_of_file: size_of_file})
+print(final_files)
+
+
+# for f, s in size_of_files:
+#     print("{} : {}mb".format(f, round(s / (1024 * 1024), 3)))
--- password_module/main.py
+++ password_module/main.py
@@ -15,17 +15,20 @@ from os.path import join
 #     # TODO
 
 
-project_root = pathlib.Path()
-dir_path = join(project_root, "theHarvester")
+dir_path = join(pathlib.Path(), "theHarvester")
 data_path = glob.glob(f"{dir_path}/**/*", recursive=True)
-string_to_match = "password:"
+string_to_match = "password"
 
 files = [i for i in data_path if os.path.isfile(i)]
+final_files = {}
 
 for file in files:
     with open(file, "rb") as fp:
         data = fp.read()
         if string_to_match.encode() in data:
             output = data.decode("ISO-8859-1")
-            pattern = re.findall(r"password: ([^\n]*)", output)
-            print(pattern)
+            pattern_to_find = re.findall(r"password\S ([^\n]*)", output)
+            # Convert list to string and remove quotes
+            match = str(pattern_to_find)[1:-1].strip("'")
+            final_files.update({"password": match, "filename": file})
+            print(final_files)
