0fb351dd094eadb7aa107327cd18211403a19595
Max Shapira <maxshapira9@gmail.com>
2022-07-30 11:59:48 +0300

    create two functions to get file type and its size

--- analyze_module/main.py
+++ analyze_module/main.py
@@ -20,25 +20,28 @@ data_path = glob.glob(f"{dir_path}/**/*", recursive=True)
 files = [i for i in data_path if os.path.isfile(i)]
 
 final_files = {}
-counts = Counter()
-
-for file in files:
-    file_type = os.path.splitext(file)[1]
-
-    if "." in file_type:
-        counts[file_type] += 1
-
-for file_type, count in counts.items():
-    final_files.update({file_type: count})
-
-# Sort the top 10 files by size
-files_ascending = sorted(files, key=lambda x: os.stat(x).st_size, reverse=True)[:10]
-
-for path_of_file in files_ascending:
-    size_of_file = os.stat(path_of_file).st_size
-    final_files.update({path_of_file: size_of_file})
-print(final_files)
 
 
-# for f, s in size_of_files:
-#     print("{} : {}mb".format(f, round(s / (1024 * 1024), 3)))
+def get_file_type():
+    counts = Counter()
+    for file in files:
+        file_type = os.path.splitext(file)[1]
+
+        if "." in file_type:
+            counts[file_type] += 1
+
+    for file_type, count in counts.items():
+        final_files.update({file_type: count})
+    return final_files
+
+
+def get_file_size():
+    # Sort the top 10 files by size
+    files_ascending = sorted(files, key=lambda x: os.stat(x).st_size, reverse=True)[:10]
+
+    for path_of_file in files_ascending:
+        size_of_file = os.stat(path_of_file).st_size
+        # Show the size in megabytes
+        size_of_file_mb = f"{str(round(size_of_file / (1024 * 1024), 3))} MB"
+        final_files.update({path_of_file: size_of_file_mb})
+    return final_files
--- controller_module/main.py
+++ controller_module/main.py
@@ -1,9 +1,10 @@
 import logging
-import messageBroker
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+# import messageBroker
 
-if __name__ == '__main__':
-    logger.info("Controller module is running and listening...")
-    # TODO
+# logging.basicConfig(level=logging.INFO)
+# logger = logging.getLogger(__name__)
+
+# if __name__ == '__main__':
+#     logger.info("Controller module is running and listening...")
+#     # TODO
