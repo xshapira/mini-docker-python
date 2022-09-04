78fbff40c601959640be7c04267c64a6b671040d
Max Shapira <maxshapira9@gmail.com>
2022-09-04 10:31:38 +0300

    cleanup comments

--- controller_module/main.py
+++ controller_module/main.py
@@ -9,9 +9,12 @@ logger = logging.getLogger(__name__)
 
 
 def on_message_received(ch, method, properties, body):
-    """Callback function when a message is received.
+    """
+    Callback function when a message is received.
     To append all our 3 dictionaries and end up with a valid JSON,
-    we load the current content into a python dict, then update this dict with the new data, and finally overwrite the JSON file to disk."""
+    we load the current content into a python dict, then update this dict
+    with the new data, and finally overwrite the JSON file to disk.
+    """
     logger.info("Received new message")
     body_dict = json.loads(body)
     logger.info(body)
@@ -33,12 +36,6 @@ def on_message_received(ch, method, properties, body):
 if __name__ == "__main__":
     try:
         logger.info("Controller module is running and listening...")
-
-        # queue = "letterbox"
-        # channel = messageBroker.receive_message(queue, on_message_received)
-        # channel.basic_consume(
-        #     queue=queue, auto_ack=True, on_message_callback=on_message_received
-        # )
 
         logger.info("Starting Consuming")
 
