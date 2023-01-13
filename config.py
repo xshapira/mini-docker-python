793dafc9634aecf03fb5e36234ba47211276eb8f
Max Shapira <maxshapira9@gmail.com>
2023-01-13 16:39:23 +0200

    fix 'line too long' error in get_settings fucntion docstring

--- config.py
+++ config.py
@@ -18,7 +18,10 @@ class Settings(BaseSettings):
 @functools.cache
 def get_settings() -> Settings:
     """
-    We're using `cache` decorator to re-use the same settings object, instead of reading it for each request. The Settings object will be created only once, the first time it's called. Then it will return the same object that was returned on the first call, again and again.
+    We're using `cache` decorator to re-use the same settings object,
+    instead of reading it for each request. The Settings object will be
+    created only once, the first time it's called. Then it will return
+    the same object that was returned on the first call, again and again.
     """
     return Settings()
 
